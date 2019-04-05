c0f33ac8a3c8e7311dbfee5eae51a135f96e28a7
Dinislam_Abdulsalamov <dinislam_abdulsalamov@epam.com>
2019-04-05 16:29:39 +0300

    change prod api endpoint

--- front/src/conf.js
+++ front/src/conf.js
@@ -1,5 +1,5 @@
 export const API_ENDPOINT = process.env.NODE_ENV === 'production'
-    ? 'http://62.109.26.114'
+    ? 'http://62.109.26.114/redis_api'
     : 'http://localhost:3000'
 export const API_ROUTES = {
     EVENT: 'event',
